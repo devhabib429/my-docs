8f722317e6f3ee154b764cdc3dfdcb1d3add35cb
Ubuntu <ubuntu@ip-172-31-83-18.ec2.internal>
2025-01-09 12:40:42 +0000

    Deploy

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,19 @@
+import Link from 'next/link';
+
 export default function Navbar() {
     return (
-      <nav className="bg-blue-600 text-white p-4">
-        <div className="container mx-auto flex justify-between">
-          <div className="text-xl font-semibold">My Blog</div>
-          <div className="space-x-4">
-            <a href="/" className="hover:text-gray-200">Home</a>
-            <a href="/about" className="hover:text-gray-200">About</a>
-          </div>
-        </div>
-      </nav>
+        <nav className="bg-blue-600 text-white p-4">
+            <div className="container mx-auto flex justify-between">
+                <div className="text-xl font-semibold">My Blog</div>
+                <div className="space-x-4">
+                    <Link href="/" passHref>
+                        <a className="hover:text-gray-200">Home</a>
+                    </Link>
+                    <Link href="/about" passHref>
+                        <a className="hover:text-gray-200">About</a>
+                    </Link>
+                </div>
+            </div>
+        </nav>
     );
-  }
-  
+}
--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
-module.exports = {
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/blogs', // Replace with your GitHub repository name
   trailingSlash: true,
-}
+};
+
+export default nextConfig;
